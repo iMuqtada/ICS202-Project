553d43a6682994945df84a800b374d381897df03
Muqtada Ismaeel <s202448860@kfupm.edu.sa>
2026-04-27 20:12:39 +0300

    LinkedStack is added

--- src/kfupm/clinic/ds/LinkedStack.java
+++ src/kfupm/clinic/ds/LinkedStack.java
@@ -2,8 +2,24 @@ package kfupm.clinic.ds;
 
 /** Students implement. */
 public class LinkedStack<T> {
-    public void push(T item) { throw new UnsupportedOperationException("TODO: LinkedStack.push"); }
-    public T pop() { throw new UnsupportedOperationException("TODO: LinkedStack.pop"); }
-    public T peek() { throw new UnsupportedOperationException("TODO: LinkedStack.peek"); }
-    public boolean isEmpty() { throw new UnsupportedOperationException("TODO: LinkedStack.isEmpty"); }
+
+    private Node<T> top;
+
+    private static class Node<T> {
+        T data; Node<T> next;
+        Node(T data, Node<T> next) { this.data = data; this.next = next; }
+    }
+
+    public void push(T item) { top = new Node<>(item, top); }
+
+    public T pop() {
+        if (top == null) return null;
+        T data = top.data;
+        top = top.next;
+        return data;
+    }
+
+    public T peek() { return (top == null) ? null : top.data; }
+
+    public boolean isEmpty() { return top == null; }
 }
