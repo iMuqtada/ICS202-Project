aba9cddb74dd67aff6dc58b4f83addcc8269aa40
Muqtada Ismaeel <s202448860@kfupm.edu.sa>
2026-04-28 04:56:10 +0300

    SinglyLinkedList is added

--- src/kfupm/clinic/ds/SinglyLinkedList.java
+++ src/kfupm/clinic/ds/SinglyLinkedList.java
@@ -1,9 +1,29 @@
 package kfupm.clinic.ds;
 
+import java.util.ArrayList;
 import java.util.List;
-
 /** Students implement. */
+
+
+/** Singly linked list supporting append and full traversal. */
 public class SinglyLinkedList<T> {
-    public void addLast(T item) { throw new UnsupportedOperationException("TODO: SinglyLinkedList.addLast"); }
-    public List<T> toList() { throw new UnsupportedOperationException("TODO: SinglyLinkedList.toList"); }
+
+    private Node<T> head, tail;
+
+    private static class Node<T> {
+        T data; Node<T> next;
+        Node(T data) { this.data = data; }
+    }
+
+    public void addLast(T item) {
+        Node<T> n = new Node<>(item);
+        if (tail == null) { head = tail = n; }
+        else              { tail.next = n; tail = n; }
+    }
+
+    public List<T> toList() {
+        List<T> list = new ArrayList<>();
+        for (Node<T> n = head; n != null; n = n.next) list.add(n.data);
+        return list;
+    }
 }
